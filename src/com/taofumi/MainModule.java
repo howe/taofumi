2f3b7e442dacc7262bef4e765ebb5c42c8592d86
cnyangqi <qi.yang.cn@gmail.com>
2011-08-18 00:28:13 +0800

    yangq

---
+++ src/com/taofumi/MainModule.java
@@ -0,0 +1,15 @@
+package com.taofumi;
+
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Modules;
+import org.nutz.mvc.annotation.Ok;
+
+@Modules(scanPackage = true)
+public class MainModule {
+
+    @At("/share")
+    @Ok("jsp:jsp.share.index")
+    public String doShare() {
+        return "";
+    }
+}
