1481f420e515b47d67207785762f57519f958eac
cnyangqi <qi.yang.cn@gmail.com>
2011-08-21 23:51:04 +0800

    1

--- src/com/taofumi/share/service/UserService.java
+++ src/com/taofumi/share/service/UserService.java
@@ -1,5 +1,21 @@
 package com.taofumi.share.service;
 
-public class UserService {
+import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.taofumi.share.dao.UserDao;
+import com.taofumi.share.entity.User;
+
+@Service
+@Transactional
+public class UserService {
+    @Resource(name = "userDao")
+    private UserDao dao;
+
+    // 添加用户
+    public void addUser(User user) {
+        dao.addUser(user);
+    }
 }
